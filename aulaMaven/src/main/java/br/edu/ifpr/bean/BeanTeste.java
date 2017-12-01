26c626541d42d7e3165fcb3722c246dda22aa7de
Erik Seyti Johansson <erikseyti@gmail.com>
2017-12-01 19:56:04 -0200

    Projeto Funcionando com CDI
    Inserindo as Strings em uma lista usando CDI

--- aulaMaven/src/main/java/br/edu/ifpr/bean/BeanTeste.java
+++ aulaMaven/src/main/java/br/edu/ifpr/bean/BeanTeste.java
@@ -1,11 +1,21 @@
 package br.edu.ifpr.bean;
 
-import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-@ManagedBean
-public class BeanTeste {
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@javax.enterprise.context.SessionScoped
+public class BeanTeste implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	private String valor;
+	private List<String> nomes = new ArrayList<String>();
 
 	public String getValor() {
 		return valor;
@@ -15,10 +25,18 @@ public class BeanTeste {
 		this.valor = valor;
 	}
 	
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+
 	public void salvar()
 	{
-		//salvar
-		valor = valor+" Boa Noite!";
+		//Salvar String em List Nomes
+		nomes.add(valor);
 	}
 
 }
